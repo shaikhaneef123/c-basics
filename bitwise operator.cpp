ef0045f51a72262123e073e4db8713d4bd5b679e
shaikhaneef123 <92719940+shaikhaneef123@users.noreply.github.com>
2021-10-21 15:15:22 +0530

    Add files via upload

---
+++ bitwise operator.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=24,b=12;
+	printf("%d\n",a|b);
+	printf("%d\n",a&b);
+	printf("%d\n",a^b);
+}
---
+++ conditional statements.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+	int (day);
+	scanf("%d",&day);
+	if(day==1)
+	{
+		printf("sunday");
+	}
+	else if(day==2)
+	{
+		printf("monday");
+	}
+	else if(day==3)
+	{
+		printf("tuesday");
+	}
+	else if(day==4)
+	{
+		printf("wednesday");
+	}
+	else if(day==5)
+	{
+		printf("thursday");
+	}
+	else if(day==6)
+	{
+		printf("friday");
+	}
+	else if(day==7)
+	{
+		printf("saturday");
+	}
+	else
+	{
+		printf("valid days");
+	}
+
+s
+}
+	
+
+
+	
+	
+	
+	
+	
+	
