1912ad6286724fe541764281a2965e12eef3eae9
shaikhaneef123 <92719940+shaikhaneef123@users.noreply.github.com>
2021-10-21 15:18:15 +0530

    Add files via upload

---
+++ haneef even.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int h;
+	scanf("%d",&h);
+	if(h>=18)
+	printf("eligible");
+	else
+	printf("not eligible");
+}
---
+++ haneef if.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,p,t;
+	scanf("%d%d%d",&a,&b,&c);
+	t=a+b+c;
+	p=((t*100)/300);
+	if(p>=90)
+	printf("a grade");
+	else if(p>=80)
+	printf("b grade");
+	else if("p>=60")
+	printf("c grade");
+	else if("p>=40")
+	printf("d grade");
+	else
+	printf("fail");
+	
+}
