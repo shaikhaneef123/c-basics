94d84e50c4947c7a464c471ad7c33648c7335962
shaikhaneef123 <92719940+shaikhaneef123@users.noreply.github.com>
2021-11-01 15:16:38 +0530

    Add files via upload

---
+++ h prime functions hhhh.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int isprime(int);
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	for(i=2;i<=n;i++)
+	{
+		if(isprime(i))
+		{
+			printf("%d ",i);
+		}
+	}
+}
+int isprime(int n)
+{
+	int i;
+	for(i=2;i<n;i++)
+	{
+	if(n%i==0)
+	{
+		return 0;
+	}
+}
+return 1;
+}
