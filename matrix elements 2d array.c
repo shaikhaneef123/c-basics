d48c9c6acf1f2fe3dccb65673151d5b7028e74d4
shaikhaneef123 <92719940+shaikhaneef123@users.noreply.github.com>
2021-11-30 14:32:31 +0530

    Add files via upload

---
+++ matrix elements 2d array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],i,j;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		scanf("%d",&a[i][j]);
+	    }
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d",a[i][j]);
+		}
+		printf("\n");
+	}
+}
